7bab7f25e55ecfdd6ca29594d471a63b148e00b0
William G <hswg94@gmail.com>
2026-01-14 17:35:08 +0800

    Refactor receive_logs.py to improve code readability by adding spacing and comments

--- receive_logs.py
+++ receive_logs.py
@@ -5,12 +5,16 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
+#declare the exchange
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
+#create a temporary queue with a random name and get the queue name
 result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue
 
+#bind the queue to the exchange
 channel.queue_bind(exchange='logs', queue=queue_name)
+
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
